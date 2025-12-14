9675cf1e2bb7f88ee2434127a46859583660c8c1
TheKy1565 <23050014@student.bdu.edu.vn>
2025-12-14 12:51:52 +0700

    Sửa lại 1 số thứ backend

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,35 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import urllib.parse
 import os
+import urllib.parse
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
+# Load file .env (chỉ chạy khi ở Local)
 load_dotenv()
 
-raw_password = os.getenv("DB_PASSWORD") 
-encoded_password = urllib.parse.quote_plus(raw_password)
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{encoded_password}@127.0.0.1:3306/NoteTakingApp"
+
+# Lấy từng thành phần từ biến môi trường
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+
+if not all([db_user, db_password, db_host, db_port, db_name]):
+    raise ValueError("Thiếu biến môi trường rồi bé ơi! Kiểm tra lại .env hoặc Render đi!")
+
+#Xử lý password
+encoded_password = urllib.parse.quote_plus(db_password)
+
+#Ghép lại thành chuỗi hoàn chỉnh (f-string)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db_user}:{encoded_password}@{db_host}:{db_port}/{db_name}"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
